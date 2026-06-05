e05024ba07cc1bf4222ae2c736573b8db401cd77
Bryan <bssanchez_usistemas@intep.edu.co>
2026-06-05 13:36:43 -0500

    version inicial

--- paginaRopa/next.config.ts
+++ paginaRopa/next.config.ts
@@ -28,15 +28,6 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-      {
-        source: "/:path*\\.(?:jpg|jpeg|png|webp|avif|svg)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=86400, stale-while-revalidate=604800",
-          },
-        ],
-      },
     ];
   },
 };
